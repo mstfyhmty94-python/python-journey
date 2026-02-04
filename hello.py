be354d220ed38e199cded5a3c36af1817ce8aaa5
MOSTAFA-HEMMATY <mstfyhmty94@gmail.com>
2026-02-04 14:50:47 +0330

    hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+# hello.py
+print("Hello GitHub! This is my first Python file!")
+print("سن من ۲۲ سال است و تازه شروع کردم!")
+print("انشاالله موفق می‌شوم! 💪")
+
+
+
+
+
+
+
+
+
+
+
+Commit new file
+Title: Add first Python file
+Description: My very first Python code on GitHub
+✔ Commit directly to the main branch
